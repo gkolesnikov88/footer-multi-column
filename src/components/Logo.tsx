7b171f07ff7acefcbb5f012b20aa797c720ee316
George Kolesnikov <gkolesnikov88@gmail.com>
2024-06-08 13:58:22 +0200

    Hover and focus styling

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -17,18 +17,32 @@ const Logo = () => {
         <div className='links-wrapper'>            
             <div className='links__column__title'>SHOP CATEGORIES</div>
             <ul className='links__list'>
-                <li className='links__list__link'>Unisex</li>
-                <li className='links__list__link'>Women</li>
-                <li className='links__list__link'>Men</li>
+                <li className='links__list__item'>
+                    <a href="/" className='links__list__link'>Unisex</a>
+                </li>
+                <li className='links__list__item'>
+                    <a href="/" className='links__list__link'>Women</a>
+                </li>
+                <li className='links__list__item'>
+                    <a href="/" className='links__list__link'>Men</a>
+                </li>
             </ul>        
         </div>
         <div className='links-wrapper'>
             <div className='links__column__title'>SHOP COLLECTIONS</div>
             <ul className='links__list'>
-                <li className='links__list__link'>Latest arrivals</li>
-                <li className='links__list__link'>Urban Oasis</li>
-                <li className='links__list__link'>Cozy Comfort</li>
-                <li className='links__list__link'>Fresh Fusion</li>
+                <li className='links__list__item'>
+                    <a href="/" className='links__list__link'>Latest arrivals</a>
+                </li>
+                <li className='links__list__item'>
+                    <a href="/" className='links__list__link'>Urban Oasis</a>
+                </li>
+                <li className='links__list__item'>
+                    <a href="/" className='links__list__link'>Cozy Comfort</a>
+                </li>
+                <li className='links__list__item'>
+                    <a href="/" className='links__list__link'>Fresh Fusion</a>
+                </li>
             </ul>
         </div>
     </div>
